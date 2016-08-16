097af3dd1aa5671d91ca681a6bedd1270f00f724
Pavel Dziashchenia <pavel_dziashchenia@epam.com>
2016-08-16 14:59:32 +0300

    done

---
+++ Exceptions_Handling_2_HW/IntParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exceptions_Handling_2_HW
+{
+    public static class IntParser
+    {
+        public static int Parse(string str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+            }
+
+            if (str.Equals(string.Empty))
+            {
+                throw new ArgumentException("Input string is empty", "str");
+            }
+
+            int result = 0;
+            int signCoefficient = 1;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]) == false)
+                {
+                    if (i == 0 && str[i].Equals('-') && str.Length > 1)
+                    {
+                        signCoefficient = -1;
+
+                        continue;
+                    }
+                    else
+                    {
+                        throw new FormatException("Incorrect format of input string");
+                    }
+                }
+
+                checked
+                {
+                    result = result * 10 + (signCoefficient * (int)char.GetNumericValue(str[i]));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Exceptions_Handling_2_HW_Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Exceptions_Handling_2_HW;
+
+namespace Exceptions_Handling_2_HW_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IntParser.Parse("-2147483649");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Exception: value < int.MinValue");
+            }
+
+            try
+            {
+                IntParser.Parse("2147483648");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Exception: value > int.MaxValue");
+            }
+
+            try
+            {
+                IntParser.Parse("sadasd");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Exception: Incorrect format");
+            }
+
+            Console.WriteLine(IntParser.Parse("256"));
+            Console.WriteLine(IntParser.Parse("-54132"));
+            Console.WriteLine(IntParser.Parse(int.MaxValue.ToString()));
+            Console.WriteLine(IntParser.Parse(int.MinValue.ToString()));
+        }
+    }
+}
